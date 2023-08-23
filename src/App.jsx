cf340ee92aa555678f6d59a7c95af2f64406b82b
Zaid Marouf <zaidmarouf15@gmail.com>
2023-08-24 03:38:54 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import MovieCard from './MovieCard';
 import SearchIcon from './assets/search.svg';
@@ -6,37 +6,37 @@ import SearchIcon from './assets/search.svg';
 const API_URL = 'https://www.omdbapi.com/?apikey=9b59d2b3';
 
 const App = () => {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState('spider');
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const searchMovies = async (title) => {
-    const response = await axios.get(`${API_URL}&s=${title}`);
-    const data = await response.data;
-    const value = await data.Search;
-    setMovies(value);
-    setIsLoading(false);
-  };
-
   useEffect(() => {
-    searchMovies('avengers');
-  }, []);
+    const searchMovies = async (title) => {
+      const response = await axios.get(`${API_URL}&s=${title}`);
+      const data = await response.data;
+      const value = await data.Search;
+      setMovies(value);
+      setIsLoading(false);
+    };
+    searchMovies(searchText);
+  }, [searchText]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setSearchText(e.target.value);
+    }
+  };
 
   return (
     <div className="app">
       <h1>MoviesDekho</h1>
 
       <div className="search">
+        <img src={SearchIcon} alt="search" />
         <input
           type="text"
           placeholder="Search Movies ..."
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => searchMovies(searchText)}
+          onKeyDown={handleKeyDown}
         />
       </div>
 
